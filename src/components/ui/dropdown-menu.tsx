59b168eacd0673f9e058f2a53ff1598b50d03bdb
Abdul1028 <rasoolas2003@gmail.com>
2025-06-25 01:25:48 +0530

    removed theme dropdown menu

--- src/components/ui/dropdown-menu.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import * as React from "react";
-import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
-
-export const DropdownMenu = DropdownMenuPrimitive.Root;
-export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
-export const DropdownMenuContent = DropdownMenuPrimitive.Content;
-export const DropdownMenuItem = DropdownMenuPrimitive.Item; 
